f5422cfbcad19652491ccabd5fed67368b778caa
jacob-a2k <jakub.a2k@gmail.com>
2025-08-14 20:14:08 +0200

    BlazingPizza from Microsoft doc Blazor

---
+++ Data/Controllers/SpecialsController.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Data.Controllers;
+
+[Route("specials")]
+[ApiController]
+public class SpecialsController : Controller
+{
+    private readonly PizzaStoreContext _db;
+    public SpecialsController(PizzaStoreContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<ActionResult<List<PizzaSpecial>>> GetSpecials()
+    {
+        return (await _db.Specials.ToListAsync())
+            .OrderByDescending(
+                s => s.BasePrice).ToList();
+    }
+}
